c457b282bb07797b3ef9382e24d60de43f2241b7
SimonP <SimonP@gmail.com>
2023-12-28 21:17:43 +0100

    updated camera

--- camera_free.gd
+++ camera_free.gd
@@ -6,6 +6,7 @@ const ALT_MULTIPLIER = 1.0 / SHIFT_MULTIPLIER
 
 
 @export_range(0.0, 1.0) var sensitivity: float = 0.25
+@export_range(0.0, 5.0) var speed: float = 1.
 
 # Mouse state
 var _mouse_position = Vector2(0.0, 0.0)
@@ -36,7 +37,7 @@ func _input(event):
 	# Receives mouse button input
 	if event is InputEventMouseButton:
 		match event.button_index:
-			MOUSE_BUTTON_RIGHT: # Only allows rotation if right click down
+			MOUSE_BUTTON_MIDDLE: # Only allows rotation if wheel down
 				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED if event.pressed else Input.MOUSE_MODE_VISIBLE)
 			MOUSE_BUTTON_WHEEL_UP: # Increases max velocity
 				_vel_multiplier = clamp(_vel_multiplier * 1.1, 0.2, 20)
@@ -83,7 +84,7 @@ func _update_movement(delta):
 		+ _velocity.normalized() * _deceleration * _vel_multiplier * delta
 
 	# Compute modifiers' speed multiplier
-	var speed_multi = 1
+	var speed_multi = speed
 	if _shift: speed_multi *= SHIFT_MULTIPLIER
 	if _alt: speed_multi *= ALT_MULTIPLIER
 
